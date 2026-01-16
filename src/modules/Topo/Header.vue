55996a31dbc49959dc89bdb2d8cefafc37d57914
Thiago Nogueira <thiagonogueira_2003@hotmail.com>
2026-01-16 20:56:36 -0300

    wip: Realizando alterações responsividade do Header

--- src/modules/Topo/Header.vue
+++ src/modules/Topo/Header.vue
@@ -142,7 +142,7 @@
         <div>
             <a 
                 href="#" 
-                class="group w-full flex items-center justify-between px-6 py-3 bg-[#141414] text-white text-xl font-bold tracking-wider transition-all duration-300 border-b-2 border-white hover:border-white/70"
+                class="group w-full flex items-center justify-between px-6 py-2 bg-[#141414] text-white text-xl font-bold tracking-wider transition-all duration-300 border-b-2 border-white hover:border-white/70"
             >
                 <span>Get a Quote</span>
                 <Svg class="text-white group-hover:translate-x-2 transition-transform duration-300" />
